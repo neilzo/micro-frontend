cd731f5d97066c9d6803c43159b5cfec5481cc80
Neil Daftary <nmd278@nyu.edu>
2020-04-10 11:04:12 -0700

    Add eslint.

--- container/src/About.js
+++ container/src/About.js
@@ -8,8 +8,8 @@ const About = () => {
       <h1>About</h1>
 
       <p>
-        Simple Restaurant Website has what you want. What you need. With no superfluous content or SEO spam (no author's
-        life stories here).
+        Simple Restaurant Website has what you want. What you need. With no superfluous content or
+        SEO spam (no author&apos;s life stories here).
       </p>
 
       <p>Our motto is: Find a recipe. Make it.</p>
